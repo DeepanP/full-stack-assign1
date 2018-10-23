5d502af19cd5b499e504aa10d13e41c708cc946c
Deepan Palaniyappan <deepalan@publicisgroupe.net>
2018-10-23 19:55:55 +0530

    changes autho

--- index.js
+++ index.js
@@ -1,22 +1,90 @@
 
-/*window.addEventListener('load', function() {
-
-    var webAuth = new auth0.WebAuth({
-      domain: 'assignment1.auth0.com',
-      clientID: 'd666790ca8e2a07b59cd',
-      responseType: 'token id_token',
-      scope: 'openid',
-      redirectUri: window.location.href
+var AUTH0_CLIENT_ID='pI2hZe0tAKEDUcq7KP7pO98pzVF54V6D';
+var AUTH0_DOMAIN='assignment1.auth0.com';
+var AUTH0_CALLBACK_URL=location.href+'/?loginCallback';
+window.addEventListener('load', function() {
+    var content = document.querySelector('.content');
+    var loadingSpinner = document.getElementById('loading');
+    content.style.display = 'block';
+    loadingSpinner.style.display = 'none';
+  
+    var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+      autoclose: true,
+      auth: {
+        redirectUrl: AUTH0_CALLBACK_URL,
+        responseType: 'token id_token',
+        params: {
+          scope: 'openid'
+        }
+      }
     });
   
-    //var loginBtn = document.getElementById('btn-login');
+    lock.on('authenticated', function(authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        setSession(authResult);
+      }
+      displayButtons();
+    });
   
-    //loginBtn.addEventListener('click', function(e) {
-      //e.preventDefault();
-      webAuth.authorize();
-    //});
+    lock.on('authorization_error', function(err) {
+      console.log(err);
+      alert('Error: ' + err.error + '. Check the console for further details.');
+      displayButtons();
+    });
   
-  });*/
+    // buttons and event listeners
+    var loginBtn = document.getElementById('btn-login');
+    var logoutBtn = document.getElementById('btn-logout');
+  
+    loginBtn.addEventListener('click', login);
+    logoutBtn.addEventListener('click', logout);
+  
+    function login() {
+      lock.show();
+    }
+  
+    function setSession(authResult) {
+      // Set the time that the access token will expire at
+      var expiresAt = JSON.stringify(
+        authResult.expiresIn * 1000 + new Date().getTime()
+      );
+      localStorage.setItem('access_token', authResult.accessToken);
+      localStorage.setItem('id_token', authResult.idToken);
+      localStorage.setItem('expires_at', expiresAt);
+    }
+  
+    function logout() {
+      // Remove tokens and expiry time from localStorage
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('id_token');
+      localStorage.removeItem('expires_at');
+      displayButtons();
+    }
+  
+    function isAuthenticated() {
+      // Check whether the current time is past the
+      // access token's expiry time
+      var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+      return new Date().getTime() < expiresAt;
+    }
+  
+    function displayButtons() {
+      var loginStatus = document.querySelector('.container h4');
+      if (isAuthenticated()) {
+        loginBtn.style.display = 'none';
+        logoutBtn.style.display = 'inline-block';
+        loginStatus.innerHTML = 'You are logged in!';
+      } else {
+        loginBtn.style.display = 'inline-block';
+        logoutBtn.style.display = 'none';
+        loginStatus.innerHTML =
+          'You are not logged in! Please log in to continue.';
+      }
+    }
+  
+    displayButtons();
+  });
+
 var userName = '';
 var repoList = [];
 var getRepoUrl = { url: 'https://api.github.com/users/:owner/repos', tokens:['owner']};
@@ -93,8 +161,8 @@ function createIssueByUrl(owner,reponame){
 function submitIssue(event){
     event.preventDefault();
     var data = {
-        "title": "Found a bug",
-        "body": "I'm having a problem with this.",
+        "title": document.getElementById('issueTitle').value,
+        "body": document.getElementById('issueDesc').value,
         "assignees": [
           userName
         ],
